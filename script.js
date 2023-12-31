7e5121366d526ef8337e3a038b4f1651edc96ec8
FreqZY <155252397+FreqZY@users.noreply.github.com>
2023-12-31 20:25:19 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-function calculate() {
+function calculate(operation) {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = 0;
+
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 === 0) {
+                alert("Division by zero is not allowed!");
+                return;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            break;
+    }
+
+    var resultElement = document.getElementById("result");
+    resultElement.textContent = "Result: " + result.toFixed(2);
+    resultElement.style.display = "block";
+}
+
+function calculateHypotenuse() {
     var sideA = parseFloat(document.getElementById("sidea").value);
     var sideB = parseFloat(document.getElementById("sideb").value);
 
@@ -9,11 +40,20 @@ function calculate() {
     resultHypotenuse.style.display = "block";
 }
 
-function convert() {
+function convertToCentimeter() {
     var meter = parseFloat(document.getElementById("meter").value);
     var centimeter = meter * 100;
 
     var resultConversion = document.getElementById("resultConversion");
     resultConversion.textContent = meter + " meters is equal to " + centimeter + " centimeters";
     resultConversion.style.display = "block";
+}
+
+function convertToFeet() {
+    var inches = parseFloat(document.getElementById("inches").value);
+    var feet = inches / 12;
+
+    var resultFeet = document.getElementById("resultFeet");
+    resultFeet.textContent = inches + " inches is equal to " + feet.toFixed(2) + " feet";
+    resultFeet.style.display = "block";
 }
